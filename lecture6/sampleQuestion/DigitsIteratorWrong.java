64c44a9dfde002a1503c62465ae13759721bf983
Xuezhi Yan <2000chrisyan@gmail.com>
2023-08-03 15:45:32 -0700

    sample exam question lec6

--- lecture6/sampleQuestion/DigitsIteratorWrong.java
+++ lecture6/sampleQuestion/DigitsIteratorWrong.java
@@ -2,11 +2,11 @@ package lecture6.sampleQuestion;
 
 import java.util.Iterator;
 
-class DigitsIterator implements Iterator<Integer> {
+class DigitsIteratorWrong implements Iterator<Integer> {
     private int value;
 
-    public DigitsIterator(int value) {
-        this.value = value * 10;
+    public DigitsIteratorWrong(int value) {
+        this.value = value;
     }
 
     public boolean hasNext() {
@@ -14,27 +14,26 @@ class DigitsIterator implements Iterator<Integer> {
     }
 
     public Integer next() {
-        this.value = this.value / 10;
         return this.value % 10;
     }
 }
 
-class Digits implements Iterable<Integer> {
+class DigitsWrong implements Iterable<Integer> {
     private int value;
 
-    public Digits(int value) {
+    public DigitsWrong(int value) {
         System.out.println("Digits of " + value);
         this.value = value;
     }
 
     public Iterator<Integer> iterator() {
-        return new DigitsIterator(this.value);
+        return new DigitsIteratorWrong(this.value);
     }
 }
 
-class ExecuteTest {
+class ExecuteTestWrong {
     public static void main(String[] args) {
-        Digits digits = new Digits(2019);
+        DigitsWrong digits = new DigitsWrong(2019);
         for (Integer digit : digits) {
             System.out.println(digit);
         }
